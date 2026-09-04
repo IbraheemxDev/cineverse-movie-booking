874c7e3aa7a1e42ed21835901271338dc231710f
IbraheemxDev <ibraheem.codes@gmail.com>
2026-09-04 05:14:38 +0500

    feat: adjust layout padding on movie page for improved responsiveness

--- src/app/(app)/movies/[id]/page.tsx
+++ src/app/(app)/movies/[id]/page.tsx
@@ -98,7 +98,7 @@ const Page = () => {
   const { movie, dateTime } = show
 
   return (
-    <div className="px-6 md:px-16 lg:px-40 pt-30 md:pt-50">
+    <div className="px-6 md:px-16 lg:px-20 pt-30 md:pt-50">
       <div className="flex flex-col md:flex-row gap-8 max-w-6xl mx-auto">
 
         {/* Movie Poster */}
